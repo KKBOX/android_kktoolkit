c3fb7fb2fee8a252d8e736f49cdce26d7df326a7
berryhuang <million31406@gmail.com>
2013-09-30 22:07:24 +0800

    Adding judgement

--- src/com/kkbox/toolkit/ui/KKServiceActivity.java
+++ src/com/kkbox/toolkit/ui/KKServiceActivity.java
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * ​http://www.apache.org/licenses/LICENSE-2.0
+ * �ttp://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -118,6 +118,8 @@ public abstract class KKServiceActivity extends KKActivity {
 	@Override
 	protected void onPause() {
 		super.onPause();
-		KKService.getDialogNotificationManager().removeListener();
+		if (KKService.isRunning()) {
+			KKService.getDialogNotificationManager().removeListener();
+		}
 	}
 }
