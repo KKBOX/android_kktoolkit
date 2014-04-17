d13fce295b5b3235482fa9b979fe4066cc912ac9
Jason ho <jasonho@kkbox.com>
2014-04-17 16:54:45 +0800

    Add resetFetchData method for KKFragment

--- src/com/kkbox/toolkit/app/KKFragment.java
+++ src/com/kkbox/toolkit/app/KKFragment.java
@@ -82,6 +82,10 @@ public abstract class KKFragment extends Fragment {
 		customLoadingView = view;
 	}
 
+	protected void resetFetchData() {
+		dataFetchedStatus = DataFetchStatus.NONE;
+	}
+
 	protected void startFetchData() {
 		if (viewMessage != null) {
 			if (customLoadingView != null) {
